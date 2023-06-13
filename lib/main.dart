5bd1e16c7ba7767b1cfc555ca24323adef7a369f
子言 朱 <461960286@qq.com>
2023-06-13 14:49:33 +0000

    save

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -75,6 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -102,6 +109,14 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            ElevatedButton(
+              child:Text("清零"),
+              onPressed:_resetCounter,
+            ),
+            Image(
+              image: AssetImage("images/1.png"),
+               width: 1000.0
+            ),
           ],
         ),
       ),
@@ -110,6 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      
     );
   }
 }
